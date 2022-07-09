26beb76c06801d86e211d9caab705b79b6503772
kotasuke55 <uverock5@gmail.com>
2022-07-09 21:09:25 +0900

    s

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    sum: 0,
+    lastTeam: '',
   },
   mutations: {
-  },
-  actions: {
-  },
-  modules: {
+    addSum(state,payload) {
+      state.sum++;
+      state.lastTeam=payload;
+      
+    }
   }
 })
